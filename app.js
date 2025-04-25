3cc802e0d37113d6fe31c3cdad6f193a03826ea1
shreyanshshardul <nikhilsinghk143@gmail.com>
2025-04-25 23:30:55 +0530

    Dino-Jump

---
+++ app.js
@@ -0,0 +1,72 @@
+const h1 = document.querySelector("h1");
+const dino = document.querySelector(".dino");
+const enemy = document.querySelector(".enemy");
+let score = document.querySelector(".score");
+let count = 0;
+
+let start = false;
+let gameInterval;
+
+document.addEventListener("click", () => {
+    if (!start) {
+        start = true;
+        h1.innerText = "Level 1";
+        moveable();
+        gameInterval = setInterval(checkCollision, 100);
+    }
+
+    // Dino jump
+    dino.classList.add("up");
+    setTimeout(() => {
+        dino.classList.remove("up");
+    }, 700);
+
+    // Score update
+    if (start) {
+        count += 5;
+        score.innerText = `Score : ${count}`;
+    }
+
+    if (count >= 50) {
+        clearInterval(gameInterval);
+        window.location.href = "index1.html";
+    }
+});
+
+function moveable() {
+    if (!enemy.classList.contains("move")) {
+        enemy.classList.add("move");
+    }
+}
+
+
+function checkCollision() {
+    const dinoRect = dino.getBoundingClientRect();
+    const enemies = document.querySelectorAll(".enemy img"); // har enemy image ko pakdo
+
+    enemies.forEach(enemy => {
+        const enemyRect = enemy.getBoundingClientRect();
+
+        if (
+            dinoRect.bottom > enemyRect.top &&
+            dinoRect.top < enemyRect.bottom &&
+            dinoRect.right > enemyRect.left &&
+            dinoRect.left < enemyRect.right
+        ) {
+            gameOver(); // collision hua toh game over
+        }
+    });
+}
+
+
+function gameOver() {
+    clearInterval(gameInterval);
+    h1.innerText = "💀 Game Over!";
+    enemy.classList.remove("move");
+    dino.classList.remove("up");
+    start = false;
+
+    setTimeout(() => {
+        window.location.reload();
+    }, 2000);
+}
